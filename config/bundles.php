5f095dbd180a01b5e07293a0fb7684132bf215dc
Ronan Lenouvel <ronan.develop@gmail.com>
2026-08-12 10:24:29 +0200

    🐛 fix(api): activer l'interface Swagger UI (/api)
    Sans Twig, API Platform désactive silencieusement Swagger UI/ReDoc/Scalar
    (pas d'erreur au démarrage, juste un 400 "format html not supported" à
    l'usage). Ajout de symfony/twig-bundle + déclaration explicite des
    formats (jsonld/json/html) dans api_platform.yaml. Titre/description de
    l'API également renseignés (résidu "Hello API Platform" du skeleton).

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
